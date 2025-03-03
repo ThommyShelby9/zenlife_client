b3f2f36c7d9a5dd5b6921f146a669e11a4d4c078
ThommyShleby9 <merluxsolitaire@gmail.com>
2025-03-03 22:21:18 +0100

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
         categories: ['health', 'lifestyle'],
         icons: [
           {
-            src: 'img/logo-512x512.png',
+            src: 'img/logo-512x512-maskable.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
